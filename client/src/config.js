e1e40347f49b5c7c0c1f3f1b3b20df8f749c71b1
jasonochiam <ochiamjason@gmail.com>
2025-11-11 14:17:55 -0600

    back to heroku

--- client/src/config.js
+++ client/src/config.js
@@ -3,7 +3,7 @@
 // In development, use the Heroku backend URL
 const API_URL = process.env.NODE_ENV === 'production' 
   ? '' // relative URLs in production
-  : 'http://localhost:8001';
-  //'https://backendserver1-ab6b6912c013.herokuapp.com';
+  : //'http://localhost:8001';
+  'https://backendserver1-ab6b6912c013.herokuapp.com';
 
 export default API_URL;
